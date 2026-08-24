de2f80c2204579fe11ebf41fd3b9e67ee37d4e91
geethikavalivarthi-arch <geethikavalivarthi@gmail.com>
2026-08-24 10:31:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Website is working");
+alert("Hello bhaai");
+console.log("Code is running");
+console.log("Code is also running");
+console.log("Code is lokking like a wow");
+var a=prompt("Whats your name dude?");
+var isTrue=confirm("Are you sure you want to continue?");
+
+if(isTrue){
+    console.log("Computer is blasting");
+}
+else{
+    console.log("Computer is not blasting");
+}
+
+console.log("your name is "+a);
+
+document.title="My first website";
+document.body.style.backgroundColor="lightgray";
+
+
---
+++ script2.js
@@ -0,0 +1,21 @@
+console.log("This is a variable page");
+var a=5;
+let b=6;
+let c="happy";
+const d="5";
+let _a="RK and Sk";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(_a);
+
+var 55a=10;
+console.log("55a");
+console.log("a+b");
+console.log("c+_a");
+console.log("b+d");
+var f=b+d;
+console.log("f")
+console.log("typeof(f)");
+
