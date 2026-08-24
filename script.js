d320d3458f6e26fe77bbe8ac0db291e865d5e56c
geethikavalivarthi-arch <geethikavalivarthi@gmail.com>
2026-08-24 10:35:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,88 @@
-// console.log("My page");
-// var a=30;
-// a=a+1;
-// a+=1;
-// console.log(a);
-// a++;
-// console.log(a);
-// var a=10;
-// console.log(a);
-// {
-//     let a=66;
-//     console.log(a);
+// function mean(a,b,c,d,e){
+//     console.log((a+b+c+d+e)/5);
 // }
+// mean(1,2,3,4,5)
+
+// const mean2=(a,b,c,d,e)=>{
+//     return(a+b+c+d+e)/5;
+// }
+// mean2
+// const square=(x)=>{
+//     return x*x;
+// }
+// let a=square(10);
 // console.log(a);
-// // let a;
-// // console.log(a);
-// const b=3.14;
-// console.log(b);
-// // b+=1;
-// console.log(b);
 
-// let x="Hello guyyyysss";
-// let y=22;
-// let z=3.55;
-// const p=true;
-// let q=undefined;
-// let r=null;
-// console.log(x,y,z,p,q,r);
-// console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+// const square2=x=> x*x;
 
-// let o={
-//     "name":"chan Bhaai",
-//     "age":24,
-//     "isMarried":true
+// function outer(){
+//     console.log("Outer function running...");
+//     function inner(){
+//         console.log("Inner function running...");
+//     }
+//     inner();
+
 // }
-// console.log(o);
-// o.salaary="1l";
-// console.log(o);
+// outer();
+
+// let str="Hello World";
+// console.log(str.length);
+
+// let str1="Hello";
+// let str2="World";
+// console.log(str1.concat(str2));
+
+// console.log(str1+str2);
+
+// let text="Hello world";
+// console.log(text.indexOf("w"));
+
+// let str="Hello world";
+// console.log(str.slice(6));
+// console.log(str.slice(0,5));
+
+// let greeting="Hello world";
+// console.log(greeting.toUppercase());
+// console.log(greeting.toLowercase());
+
+// let message='Hello,World!';
+// console.log(message);
+
+// let name="Abhinav";
+// let greeting="Hello,"+name+ "! Welcome to JavaScript."
+// console.log(greeting);
+
+// let a=10;
+// let b=5;
+// // let c=a+b;
+// console.log(`The sum of ${a}and${b}is ${a+b}`)
+
+// let oldway="This is line 1.\n"+
+//            "This is line 2.\n"+
+//            "This is line 3.";
+// console.log(oldway);   
+
+let message=`This is line1.
+This is line 2.
+This is line 3.`;
+console.log(message);
+
+let name="Abhinav";
+let age=22;
+
+let html=`
+ <div>
+  <h2>${name}</h2>
+  <p>Age:${age}</p>
+ </div>
+`;
+console.log(html);
+
+var myArray=[1,"Hello",[2,3]];
+myArray.splice(1,1,"Hello world",[4,5]);
+console.log(myArra
 
 
-// let student={
-//     name:prompt("Geethika"),
-//     phone:prompt("7801143218"),
-//     marks:prompt("250"), 
-// }
-// console.log("name:Geethika");
-// console.log("phone:7801143218");
-// console.log("marks:250");
 
-let a=20;
-let b=5;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a**b);
-console.log(a/b);
-console.log(a%b);
-console.log(a+=b);
-console.log(a-=b);
-console.log(a/=b);
-console.log(a==b);
-console.log(a%=b);
-let num=21
-if (num%2==0){
-    console.log("Even");
-}
-else{
-    console.log("ODD");
-}
-let age=24
-if(age==18){
-    console.log("You can drive");
-}
-else if(age==0){
-    console.log("Enti comedy aaah!");
-}
-else if(age==1){
-    console.log("do one thing..");
-}
-else{
-    console.log("No you cant drive");
-}
-let new_num1=5;
-let new_num2="5";
-console.log(new_num1,new_num2);
-console.log(new_num1==new_num2);
-console.log(new_num1===new_num2);
+
+
