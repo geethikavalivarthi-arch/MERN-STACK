9643ec14d2e342b7e9a7b70443fe5717f10cb86b
geethikavalivarthi-arch <geethikavalivarthi@gmail.com>
2026-08-24 10:32:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,87 @@
-console.log("Website is working");
-alert("Hello bhaai");
-console.log("Code is running");
-console.log("Code is also running");
-console.log("Code is lokking like a wow");
-var a=prompt("Whats your name dude?");
-var isTrue=confirm("Are you sure you want to continue?");
+// console.log("My page");
+// var a=30;
+// a=a+1;
+// a+=1;
+// console.log(a);
+// a++;
+// console.log(a);
+// var a=10;
+// console.log(a);
+// {
+//     let a=66;
+//     console.log(a);
+// }
+// console.log(a);
+// // let a;
+// // console.log(a);
+// const b=3.14;
+// console.log(b);
+// // b+=1;
+// console.log(b);
 
-if(isTrue){
-    console.log("Computer is blasting");
+// let x="Hello guyyyysss";
+// let y=22;
+// let z=3.55;
+// const p=true;
+// let q=undefined;
+// let r=null;
+// console.log(x,y,z,p,q,r);
+// console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+// let o={
+//     "name":"chan Bhaai",
+//     "age":24,
+//     "isMarried":true
+// }
+// console.log(o);
+// o.salaary="1l";
+// console.log(o);
+
+
+// let student={
+//     name:prompt("Geethika"),
+//     phone:prompt("7801143218"),
+//     marks:prompt("250"), 
+// }
+// console.log("name:Geethika");
+// console.log("phone:7801143218");
+// console.log("marks:250");
+
+let a=20;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a**b);
+console.log(a/b);
+console.log(a%b);
+console.log(a+=b);
+console.log(a-=b);
+console.log(a/=b);
+console.log(a==b);
+console.log(a%=b);
+let num=21
+if (num%2==0){
+    console.log("Even");
 }
 else{
-    console.log("Computer is not blasting");
+    console.log("ODD");
 }
-
-console.log("your name is "+a);
-
-document.title="My first website";
-document.body.style.backgroundColor="lightgray";
-
-
+let age=24
+if(age==18){
+    console.log("You can drive");
+}
+else if(age==0){
+    console.log("Enti comedy aaah!");
+}
+else if(age==1){
+    console.log("do one thing..");
+}
+else{
+    console.log("No you cant drive");
+}
+let new_num1=5;
+let new_num2="5";
+console.log(new_num1,new_num2);
+console.log(new_num1==new_num2);
+console.log(new_num1===new_num2);
